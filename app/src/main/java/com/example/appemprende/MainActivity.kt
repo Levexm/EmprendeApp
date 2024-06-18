5dfd60768faaf2dea654797d6a40db8b632f4036
Levexm <156109384+Levexm@users.noreply.github.com>
2024-06-18 10:34:58 +0200

    menu inferior terminado, menu lateral falta por corregir la imagen encima de los items

--- app/src/main/java/com/example/appemprende/MainActivity.kt
+++ app/src/main/java/com/example/appemprende/MainActivity.kt
@@ -1,20 +1,93 @@
 package com.example.appemprende
 
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.example.appemprende.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var fragmentManager: FragmentManager
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val toggle = ActionBarDrawerToggle(this, binding.drawerLayout, binding.toolbar, R.string.nav_open, R.string.nav_close)
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        binding.navigationDrawer.setNavigationItemSelectedListener(this)
+
+        binding.bottomNavigation.background = null
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.bottom_home -> openFragment(HomeFragment())
+                R.id.bottom_espacios -> openFragment(EspaciosFragment())
+                R.id.bottom_empleo -> openFragment(EmpleoFragment())
+                R.id.bottom_formacion -> openFragment(FormacionFragment())
+                //R.id.bottom_videos -> openFragment(VideosFragment())
+                R.id.bottom_contacto -> openFragment(ContactoFragment())
+            }
+            true
         }
+        fragmentManager = supportFragmentManager
+        openFragment(HomeFragment())
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_home -> {
+                openFragment(HomeFragment())
+            }
+
+            R.id.nav_espacios -> {
+                openFragment(EspaciosFragment())
+            }
+
+            R.id.nav_empleo -> {
+                openFragment(EmpleoFragment())
+            }
+            R.id.nav_formacion -> {
+                openFragment(FormacionFragment())
+            }
+            R.id.nav_videos -> {
+                openFragment(VideosFragment())
+            }
+            R.id.nav_contacto -> {
+                openFragment(ContactoFragment())
+            }
+        }
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+
+    override fun onBackPressed() {
+        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    private fun openFragment(fragment: Fragment) {
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container, fragment)
+        fragmentTransaction.commit()
     }
 }
