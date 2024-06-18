d1614afbefc45facea65310075eeb9b537244276
Levexm <156109384+Levexm@users.noreply.github.com>
2024-06-18 12:28:48 +0200

    Revert "Merge branch 'letizia'"
    This reverts commit 1af367ac68f6c6f0d1dd6831c46aeddf3119dcf0, reversing
    changes made to 24bec743d64a7b7217f169fca88879142bc14ea6.

--- app/src/main/java/com/example/appemprende/MainActivity.kt
+++ app/src/main/java/com/example/appemprende/MainActivity.kt
@@ -2,9 +2,13 @@ package com.example.appemprende
 
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
@@ -31,36 +35,47 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
         binding.bottomNavigation.background = null
         binding.bottomNavigation.setOnItemSelectedListener { item ->
-            handleNavigation(item.itemId)
+            when (item.itemId) {
+                R.id.bottom_home -> openFragment(HomeFragment())
+                R.id.bottom_espacios -> openFragment(EspaciosFragment())
+                R.id.bottom_empleo -> openFragment(EmpleoFragment())
+                R.id.bottom_formacion -> openFragment(FormacionFragment())
+                //R.id.bottom_videos -> openFragment(VideosFragment())
+                R.id.bottom_contacto -> openFragment(ContactoFragment())
+            }
             true
         }
-
         fragmentManager = supportFragmentManager
         openFragment(HomeFragment())
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        handleNavigation(item.itemId)
+        when (item.itemId) {
+            R.id.nav_home -> {
+                openFragment(HomeFragment())
+            }
+
+            R.id.nav_espacios -> {
+                openFragment(EspaciosFragment())
+            }
+
+            R.id.nav_empleo -> {
+                openFragment(EmpleoFragment())
+            }
+            R.id.nav_formacion -> {
+                openFragment(FormacionFragment())
+            }
+            R.id.nav_videos -> {
+                openFragment(VideosFragment())
+            }
+            R.id.nav_contacto -> {
+                openFragment(ContactoFragment())
+            }
+        }
         binding.drawerLayout.closeDrawer(GravityCompat.START)
         return true
     }
 
-    private fun handleNavigation(itemId: Int) {
-        when (itemId) {
-            R.id.nav_home -> openFragment(HomeFragment())
-            R.id.nav_espacios -> openFragment(EspaciosFragment())
-            R.id.nav_empleo -> openFragment(EmpleoFragment())
-            R.id.nav_formacion -> openFragment(FormacionFragment())
-            R.id.nav_videos -> openFragment(VideosFragment())
-            R.id.nav_contacto -> openFragment(ContactoFragment())
-        }
-    }
-
-    private fun openFragment(fragment: Fragment) {
-        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.fragment_container, fragment)
-        fragmentTransaction.commit()
-    }
 
     override fun onBackPressed() {
         if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
@@ -68,5 +83,11 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         } else {
             super.onBackPressed()
         }
+    }
+
+    private fun openFragment(fragment: Fragment) {
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container, fragment)
+        fragmentTransaction.commit()
     }
 }
